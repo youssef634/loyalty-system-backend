08d1fd5ced8262806e140716a9bd4eb4c9485ef1
youssef634 <youssef.khaled8666@gmail.com>
2025-09-26 18:48:28 +0300

    .

--- src/users/managers.module.ts
+++ src/users/managers.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service/prisma.service';
 import { ManagersService } from './managers.service';
 import { ManagersController } from './managers.controller';
-import { RolesModule } from '@src/roles/roles.module';
+import { RolesModule } from '../roles/roles.module';
 
 @Module({
   imports: [RolesModule],
